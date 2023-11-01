8564752b7c96577f06b45eed91777b3e3bc6a24b
Astrid <35829@ma-web.nl>
2023-11-02 00:08:03 +0100

    0605 multiwheres

---
+++ 06_php_prepared_statements/multiwhere.php
@@ -0,0 +1,53 @@
+<?php
+function FindLeerlingen($conn)
+{
+    $q = "select * from leerlingen;";
+    $stmt = $conn->prepare($q);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if($result)
+    {
+        for($i=0; $i < $result->num_rows; $i++)
+        {
+            $row = $result->fetch_assoc();
+            print_r($row);
+            echo '<br>';
+        }
+    }
+}
+
+function FindLeerling($conn,$name)
+{
+    $q = "select * from leerlingen where naam = '$name';";
+    $stmt = $conn->prepare($q);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if($result)
+    {
+        for($i=0; $i < $result->num_rows; $i++)
+        {
+            $row = $result->fetch_assoc();
+            print_r($row);
+            echo '<br>';
+        }
+    }
+}
+
+include_once 'dbdata.php';
+$conn = new mysqli($databaseServer, $user, $pass, $schema);
+if($conn->connect_error)
+{
+    die("error try again\r\n");
+}
+
+echo "connected to database\r\n";
+echo '<br>';
+$naam = "mario";
+$leeftijd = "18";
+$sql = "SELECT * FROM leerlingen WHERE naam=? or leeftijd=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("si", $naam, $leeftijd);
+$stmt->execute();
+$result = $stmt->get_result();
+var_dump($result->fetch_all(MYSQLI_ASSOC));
+$conn->close();
